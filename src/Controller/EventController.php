716655114d4304a485b96d38fa0c7aebc1364743
prudhomme victor <victor.prudhomme60600@gmail.com>
2022-10-19 09:54:35 +0200

    feat(event): add possibility to display events details (back) (#22)

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class EventController extends AbstractController
 {
     #[Route('/event/{id}', name: 'see_event')]
-    public function seeEvent($id): Response
+    public function seeEvent($id, EntityManagerInterface $em): Response
     {
+        $event = $em->getRepository(Event::class)->findOneBy(['id' => $id]);
+
+        if ($event->isIsAdmin() === false) {
+            $event = ($event->getCreatedBy() == $this->getUser()) ? $event : null;
+        }
+
+
+        if ($event === null) {
+            $this->addFlash('error', 'Événement introuvable');
+            return $this->redirectToRoute('dashboard');
+        }
+
         return $this->render('event/index.html.twig', [
-            'controller_name' => 'Event => '. $id,
+            'event' => $event,
         ]);
     }
 }
